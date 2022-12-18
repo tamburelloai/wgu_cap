44efab6641ef9b9bbcf739af68fa2fffff050d35
tamburelloai <98289017+tamburelloai@users.noreply.github.com>
2022-12-18 14:57:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -194,8 +194,6 @@ with tab2:
 
     f2.update_layout(template='streamlit')
     
-    st.write(tomorrowReg)
-    st.write(len(future), len(tomorrowReg))
     days_ahead = st.select_slider(
         'Forecast Length (Days)',
         options=[1, 2, 3, 4, 5], key='days_ahead')
